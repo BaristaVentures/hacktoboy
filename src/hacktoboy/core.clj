8fc320bf3845d00ca5d8142fc86b18ea12ee3f08
Daniel Restrepo Montoya <daniel.restrepo@gmail.com>
2016-11-01 18:00:19 -0500

    Initial playground github api
    Parsing Json to Clojure
    Filter User Map to get events_url

--- src/hacktoboy/core.clj
+++ src/hacktoboy/core.clj
@@ -1,6 +1,15 @@
-(ns hacktoboy.core)
+(ns hacktoboy.core
+  (:require [cheshire.core :refer :all]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def base-url "https://api.github.com")
+
+(defn parse->clojure [url]
+  "Parse Json to Clojure"
+  (parse-string (slurp url)))
+
+(defn getValue [key user]
+  "Get the value given a key"
+  (user key))
+
+;Get Url Events of Barista Public Members
+(map (partial getValue "events_url") (parse->clojure (str base-url "/orgs/BaristaVentures/members")))
