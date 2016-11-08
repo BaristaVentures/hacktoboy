8aea71c311ab4dc6b6f2350f6ea9d991c8f26435
Daniel Restrepo Montoya <daniel.restrepo@gmail.com>
2016-11-08 16:03:37 -0500

    Heavy Refactor

--- src/hacktoboy/core.clj
+++ src/hacktoboy/core.clj
@@ -19,37 +19,21 @@
   ([org-name token]
     "Get Url Events for all Member for org-ame. Requires token and be member"
       (map (partial get-value "events_url") (parse->clojure (str base-url "/orgs/" org-name "/members?access_token=" token)))))
-                          
-
-(defn get-pullrequest [event]
-  (let [type (get event "type")
-        actor (get-in event ["actor" "login"])
-        repo (get-in event ["repo" "name"])
-        action (get-in event ["payload" "action"])
-        date (get event "created_at")]
-    (if (= type "PullRequestEvent")
-      (if (.startsWith date "2016-10")
-        (hash-map :actor actor :repo repo :date date)))))
-
-(defn clean-arr [arr]
-  (loop [init arr
-         final '()]
-    (if (empty? init)
-      final
-      (let [[element & remaining] init]
-        (recur remaining
-               (if (= element nil)
-                 final
-                 (conj final element)))))))
-
-(defn count-pr [arr-pr]
-  (if (> (count arr-pr) 0)
-    {:user (get (first arr-pr) :actor) :score (count arr-pr) :lastpr (get (first arr-pr) :date)}))
+                    
+(defn count-user-pr [arr]
+  (loop [[h & t] arr prs '()]
+    (if (empty? h)
+      (if (> (count prs) 0)
+        {:user (:actor (first prs)) :score (count prs) :last (:date (last prs))})
+      (recur t (if (and (= (get h "type") "PullRequestEvent") (.startsWith (get h "created_at") "2016-10"))
+                 (conj prs {:actor (get-in h ["actor" "login"]) :repo (get-in h ["repo" "name"]) :date (get h "created_at")})
+                 prs)))))
 
 ;;Get Members Score
 (defn -main [& args]
   (println "User - Score")
-  (apply println (map (fn [m] (str "\n" (get m :user) " - " (get m :score)))
-       (sort-by :score > (filter (fn [x] (> (count x) 0))
-           (map count-pr (map clean-arr (map (fn [arr] (map get-pullrequest arr))
-               (map parse->clojure (apply members-url-events args))))))))))
+  (apply println (map (fn [m] (str "\n" (:user m) " - " (:score m)))
+                      (sort-by :score > (filter (fn [x] (> (count x) 0))
+                                                (map count-user-pr
+                                                     (map parse->clojure (apply members-url-events args))))))))
+
