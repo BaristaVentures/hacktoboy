cb3f5256048f7338d7b243e5836f30899965047b
Daniel Restrepo Montoya <daniel.restrepo@gmail.com>
2016-11-15 08:33:20 -0500

    Refactor using Reduce in count-user-pr function

--- src/hacktoboy/core.clj
+++ src/hacktoboy/core.clj
@@ -19,21 +19,25 @@
   ([org-name token]
     "Get Url Events for all Member for org-ame. Requires token and be member"
       (map (partial get-value "events_url") (parse->clojure (str base-url "/orgs/" org-name "/members?access_token=" token)))))
-                    
-(defn count-user-pr [arr]
-  (loop [[{type "type" date "created_at" {name "name"} "repo" {login "login"} "actor"} & t] arr prs '()]
-    (if (empty? type)
-      (when (> (count prs) 0)
-        {:user (:actor (first prs)) :score (count prs) :last (:date (last prs))})
-      (recur t (if (and (= type "PullRequestEvent") (.startsWith date "2016-10"))
-                 (conj prs {:actor login :repo name :date date})
-                 prs)))))
+
+(defn count-user-pr [user-events]
+  "Count User Pull Request Events"
+  (if-let [user-score
+           (reduce (fn [final-prs pr]
+                     (let [{type "type" date "created_at" {name "name"} "repo" {login "login"} "actor"} pr]
+                       (if (and (= type "PullRequestEvent") (.startsWith date "2016-10"))
+                         (conj final-prs {:actor login :repo name :date date})
+                         final-prs)))
+                   '()
+                   user-events)]
+    {:user (:actor (first user-score)) :score (count user-score) :last (:date (last user-score))}))
 
 ;;Get Members Score
 (defn -main [& args]
   (println "User - Score")
   (apply println (map (fn [m] (str "\n" (:user m) " - " (:score m)))
-                      (sort-by :score > (filter (fn [x] (> (count x) 0))
+                      (sort-by :score > (filter (fn [x] (> (:score x) 0))
                                                 (map count-user-pr
                                                      (map parse->clojure (apply members-url-events args))))))))
+
 
